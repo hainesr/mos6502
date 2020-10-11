902f45dd0ea29a75b7a9677bc50def8157d871bc
Robert Haines <rhaines@manchester.ac.uk>
2020-10-11 15:21:02 +0100

    Add the ability to set individual CpuFlags.
    Slightly breaks with traditional ruby truthy/falsey-ness to allow `0`
    as a false value. This is more in line with what one would expect of
    hardware.

--- lib/mos6502/cpu_flags.rb
+++ lib/mos6502/cpu_flags.rb
@@ -10,28 +10,56 @@ module Mos6502
       reset!
     end
 
+    def carry=(flag)
+      @carry = set_flag(flag)
+    end
+
     def carry?
       @carry
+    end
+
+    def zero=(flag)
+      @zero = set_flag(flag)
     end
 
     def zero?
       @zero
     end
 
+    def interupt_disable=(flag)
+      @interupt_disable = set_flag(flag)
+    end
+
     def interupt_disable?
       @interupt_disable
+    end
+
+    def decimal_mode=(flag)
+      @decimal_mode = set_flag(flag)
     end
 
     def decimal_mode?
       @decimal_mode
     end
 
+    def break=(flag)
+      @break = set_flag(flag)
+    end
+
     def break?
       @break
     end
 
+    def overflow=(flag)
+      @overflow = set_flag(flag)
+    end
+
     def overflow?
       @overflow
+    end
+
+    def negative=(flag)
+      @negative = set_flag(flag)
     end
 
     def negative?
@@ -46,6 +74,12 @@ module Mos6502
       @break = false
       @overflow = false
       @negative = false
+    end
+
+    private
+
+    def set_flag(flag)            # rubocop:disable Naming/AccessorMethodName
+      flag == 0 ? false : !!flag  # rubocop:disable Style/NumericPredicate
     end
   end
 end
--- test/cpu_flags_test.rb
+++ test/cpu_flags_test.rb
@@ -21,4 +21,56 @@ class Mos6502::CpuFlagsTest < Minitest::Test
     refute(@flags.overflow?)
     refute(@flags.zero?)
   end
+
+  def test_set_flags
+    [
+      [true, true],
+      [false, false],
+      [1, true],
+      [0, false],
+      [:what?, true],
+      [nil, false]
+    ].each do |flag, set|
+      @flags.carry = flag
+      assert_equal(set, @flags.carry?)
+
+      @flags.zero = flag
+      assert_equal(set, @flags.zero?)
+
+      @flags.interupt_disable = flag
+      assert_equal(set, @flags.interupt_disable?)
+
+      @flags.decimal_mode = flag
+      assert_equal(set, @flags.decimal_mode?)
+
+      @flags.break = flag
+      assert_equal(set, @flags.break?)
+
+      @flags.overflow = flag
+      assert_equal(set, @flags.overflow?)
+
+      @flags.negative = flag
+      assert_equal(set, @flags.negative?)
+    end
+  end
+
+  def test_reset
+    @flags.carry = true
+    @flags.zero = true
+    @flags.interupt_disable = true
+    @flags.decimal_mode = true
+    @flags.break = true
+    @flags.overflow = true
+    @flags.negative = true
+
+    @flags.reset!
+
+    refute(@flags.break?)
+    refute(@flags.carry?)
+    refute(@flags.decimal_mode?)
+    refute(@flags.interupt_disable?)
+    refute(@flags.negative?)
+    refute(@flags.overflow?)
+    refute(@flags.zero?)
+  end
 end
