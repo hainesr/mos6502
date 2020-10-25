83a45ac7dd48477f362ab13b3d6c0e4336cf39bf
Robert Haines <rhaines@manchester.ac.uk>
2020-10-25 17:20:53 +0000

    Refactor the running of external test suites.
    Now they only run if they are specifically turned on.

--- test/suites/klaus2m5/run.rb
+++ test/suites/klaus2m5/run.rb
@@ -7,12 +7,11 @@
 require 'test_helper'
 require 'mos6502'
 
-class Mos6502::Klaus2m5Test < Minitest::Test
+class Mos6502::Suites::Klaus2m5 < Mos6502::Suites::Runner
   IMAGE_FILE = '6502_functional_test.bin'
   IMAGE_PATH = File.expand_path(
     File.join('6502_65C02_functional_tests', 'bin_files', IMAGE_FILE), __dir__
   )
-  OUTPUT = !ENV['MOS6502_KLAUS2M5_OUTPUT'].to_i.zero?
 
   def test_6502
     image = File.read(IMAGE_PATH).bytes
@@ -22,7 +21,7 @@ class Mos6502::Klaus2m5Test < Minitest::Test
     last_pc = 0
     message = loop do
       last_pc = cpu.pc
-      puts cpu.inspect if OUTPUT
+      puts cpu.inspect if output == :debug
 
       begin
         cpu.step
@@ -34,7 +33,7 @@ class Mos6502::Klaus2m5Test < Minitest::Test
       break 'Reached a jump/break trap.' if last_pc == cpu.pc
     end
 
-    if OUTPUT
+    unless output == :off
       puts message
       puts "Memory at break point (PC: 0x#{last_pc.to_s(16)}):"
       puts image[last_pc, 8].map { "0x#{_1.to_s(16)}" }.join(', ')
---
+++ test/suites/suites_test.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+# Copyright (c) 2020, Robert Haines.
+#
+# Licensed under the BSD License. See LICENCE for details.
+
+require 'test_helper'
+
+module Mos6502::Suites
+  extend self # rubocop:disable Style/ModuleFunction
+  attr_accessor :test_suites
+
+  ENV_NAMESPACE = :mos6502
+  TEST_SUITE_DIR = 'suites'
+  TEST_SUITE_RUNNER = 'run.rb'
+  TEST_SUITES = [:klaus2m5].freeze
+
+  # Turn on individual test suites?
+  def init_test_suites!
+    @test_suites = {}
+
+    TEST_SUITES.each do |suite, _|
+      env = [ENV_NAMESPACE, suite].map(&:to_s).join('_').upcase
+      @test_suites[suite] = ENV[env].nil? ? :off : ENV[env].intern
+    end
+  end
+
+  init_test_suites!
+
+  # A superclass for test suite runners to access configured options.
+  class Runner < Minitest::Test
+    def self.suite_name
+      name.split('::').last.downcase
+    end
+
+    def output
+      Mos6502::Suites.test_suites[self.class.suite_name.intern]
+    end
+  end
+end
+
+# Load test suites that we want to run.
+Mos6502::Suites.test_suites.each do |suite, status|
+  next if status == :off
+
+  path = File.join(suite.to_s, Mos6502::Suites::TEST_SUITE_RUNNER)
+  require_relative path
+end
